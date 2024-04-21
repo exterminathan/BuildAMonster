bc74f85356449778957841a6efd92529b23d55e4
Nathan Shturm <exterminathan@gmail.com>
2024-04-20 22:48:56 -0700

    Finished Monster.js
    added all required functionality

--- src/Scenes/Monster.js
+++ src/Scenes/Monster.js
@@ -1,7 +1,7 @@
 class Monster extends Phaser.Scene {
     constructor() {
         super("monsterScene");
-        this.my = {sprite: {}};  // Create an object to hold sprite bindings
+        this.my = {sprite: {}}; 
 
         //Create constants for the monster location
         this.bodyX = 300;
@@ -9,7 +9,6 @@ class Monster extends Phaser.Scene {
         
     }
 
-    // Use preload to load art and sound assets before the scene starts running.
     preload() {
         this.load.setPath("./assets/");
 
@@ -21,35 +20,70 @@ class Monster extends Phaser.Scene {
     }
 
     create() {
-        let my = this.my;   // create an alias to this.my for readability
+        let my = this.my; 
         
-        
+        this.createMonsterSprites();
 
-        my.sprite.armLeft = this.add.sprite(this.bodyX - 90, this.bodyY, "monsterParts", "arm_darkA.png").setFlipX(true);
-        my.sprite.armRight = this.add.sprite(this.bodyX + 90, this.bodyY, "monsterParts", "arm_darkA.png");
+        this.cursors = this.input.keyboard.addKeys({
+            smile: Phaser.Input.Keyboard.KeyCodes.S,
+            fangs: Phaser.Input.Keyboard.KeyCodes.F,
+            left: Phaser.Input.Keyboard.KeyCodes.A,
+            right: Phaser.Input.Keyboard.KeyCodes.D
+        })
+ 
+    }
 
+    createMonsterSprites() {
+        let my = this.my;
+
+        //arms
+        my.sprite.armLeft = this.add.sprite(this.bodyX - 100, this.bodyY + 20, "monsterParts", "arm_darkA.png").setFlipX(true).setAngle(20);
+        my.sprite.armRight = this.add.sprite(this.bodyX + 100, this.bodyY + 20, "monsterParts", "arm_darkA.png").setAngle(-20);
+  
+
+        //legs
+        my.sprite.legLeft = this.add.sprite(this.bodyX - 60, this.bodyY + 110, "monsterParts", "leg_whiteC.png").setFlipX(true); // Flipped for left side
+        my.sprite.legRight = this.add.sprite(this.bodyX + 60, this.bodyY + 110, "monsterParts", "leg_whiteC.png");
+
+        //body
         my.sprite.body = this.add.sprite(this.bodyX, this.bodyY, "monsterParts", "body_greenD.png");
 
-        my.sprite.legLeft = this.add.sprite(this.bodyX - 20, this.bodyY + 60, "monsterParts", "leg_left.png").setFlipX(true); // Flipped for left side
-        my.sprite.legRight = this.add.sprite(this.bodyX + 20, this.bodyY + 60, "monsterParts", "leg_right.png");
-        
-        // Add one or more eyes
-        my.sprite.eye = this.add.sprite(this.bodyX, this.bodyY - 30, "monsterParts", "eye.png");
-        
-        // Add a smiling mouth
-        my.sprite.mouthSmile = this.add.sprite(this.bodyX, this.bodyY + 10, "monsterParts", "mouth_smile.png");
-        
-        // Add head accessories, two or more
-        my.sprite.accessory1 = this.add.sprite(this.bodyX, this.bodyY - 50, "monsterParts", "accessory1.png");
-        my.sprite.accessory2 = this.add.sprite(this.bodyX, this.bodyY - 50, "monsterParts", "accessory2.png");
-        
+        //eye
+        my.sprite.eye = this.add.sprite(this.bodyX, this.bodyY - 30, "monsterParts", "eye_psycho_light.png");
+
+        //mouth
+        my.sprite.mouth = this.add.sprite(this.bodyX, this.bodyY + 60, "monsterParts", "mouth_closed_happy.png");
+
+
+        //accessories
+        my.sprite.antennaR = this.add.sprite(this.bodyX + 40, this.bodyY - 105, "monsterParts", "detail_red_antenna_large.png");
+        my.sprite.antennaL = this.add.sprite(this.bodyX - 40, this.bodyY - 105, "monsterParts", "detail_red_antenna_large.png").setFlipX(true);
+
+        my.sprite.nose = this.add.sprite(this.bodyX, this.bodyY + 23, "monsterParts", "nose_yellow.png");
 
     }
 
     update() {
-        let my = this.my;    // create an alias to this.my for readability
+        let my = this.my;
 
-       
+
+        //Mouth Changes
+        if (this.cursors.smile.isDown) {
+            my.sprite.mouth.setTexture("monsterParts", "mouthA.png").setFlipY(false);
+        } else if (this.cursors.fangs.isDown) {
+            my.sprite.mouth.setTexture("monsterParts", "mouth_closed_fangs.png").setFlipY(true);
+        }
+
+        //Movement
+        if (this.cursors.left.isDown) {
+            for (let part in my.sprite) {
+                my.sprite[part].x -= 2;
+            }
+        } else if (this.cursors.right.isDown) {
+            for (let part in my.sprite) {
+                my.sprite[part].x += 2;
+            }
+        }
     }
 
 }
